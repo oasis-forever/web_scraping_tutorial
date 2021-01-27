b575e7e63973262ee1136e0628e03cfa7bdcfe23
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-01-27 12:19:14 +0900

    Commit lecturer info

--- lib/web_scrape.py
+++ lib/web_scrape.py
@@ -9,3 +9,24 @@ password = chrome.find_element_by_id("password")
 password.send_keys("kohei")
 login = chrome.find_element_by_id("login-btn")
 login.click()
+
+# Lecturer Info
+ths = chrome.find_elements_by_tag_name("th")
+keys = []
+for th in ths:
+    keys.append(th.text)
+
+tds = chrome.find_elements_by_tag_name("td")
+vals = []
+for td in tds:
+    if "\n" in td.text:
+        vals.append(td.text.replace("\n", "、"))
+    else:
+        vals.append(td.text)
+
+profile = {}
+for i in range(len(keys)):
+    profile[keys[i]] = vals[i]
+
+print(profile)
+# => {'講師名': '今西 航平', '所属企業': '株式会社キカガク', '生年月日': '1994年7月15日', '出身': '千葉県', '趣味': 'バスケットボール、読書、ガジェット集め'}
