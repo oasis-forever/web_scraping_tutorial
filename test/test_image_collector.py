5d1815c242e377c54fa999510423f4c99888fce5
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-01-29 14:56:46 +0900

    Update TestTextExtractor#test_save_images

--- test/test_image_collector.py
+++ test/test_image_collector.py
@@ -18,8 +18,31 @@ class TestTextExtractor(unittest.TestCase):
         i = 1
         for image in images:
             self.image_collector.save_image(image, "../img/image_{:0=2}.jpg".format(i))
-            self.assertEqual(True, path.exists("../img/image_{:0=2}.jpg".format(i)))
             i += 1
+        self.assertEqual(True, path.exists("../img/image_01.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_02.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_03.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_04.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_05.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_06.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_07.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_08.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_09.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_10.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_11.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_12.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_13.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_14.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_15.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_16.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_17.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_18.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_19.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_20.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_21.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_22.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_23.jpg".format(i)))
+        self.assertEqual(True, path.exists("../img/image_24.jpg".format(i)))
 
 if __name__ == "__main__":
     unittest.main()
