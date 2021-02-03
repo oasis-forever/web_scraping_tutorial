d4fff14d7b283ca2cf63c5bce5ee48deaf303977
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-02-03 12:59:36 +0900

    Refactor InfoCollector

--- lib/info_collector.py
+++ lib/info_collector.py
@@ -5,22 +5,28 @@ sys.path.append("./concerns")
 from list_handler import *
 
 class InfoCollector:
-    def __init__(self, url):
+    def __init__(self, base_url):
         self.chrome = webdriver.Chrome(executable_path="../exec/chromedriver.exe")
+        self.base_url = base_url
+
+    def _get_url(self, query_str=""):
+        url = self.base_url + query_str
         self.chrome.get(url)
 
     def _get_ranking_items(self):
         elem_ranking_items = self.chrome.find_elements_by_class_name("u_categoryTipsItem")
         return elem_ranking_items
 
-    def get_titles(self):
+    def get_titles(self, query_str):
+        self._get_url(query_str)
         elem_titles = self.chrome.find_elements_by_class_name("u_title")
         titles = []
         for elem_title in elem_titles:
             titles.append(elem_title.text.split("\n")[-1])
         return titles
 
-    def get_evaluations(self):
+    def get_evaluations(self, query_str):
+        self._get_url(query_str)
         elem_rank_boxes = self.chrome.find_elements_by_class_name("u_rankBox")
         evaluations = []
         for elem_rank_box in elem_rank_boxes:
@@ -28,21 +34,24 @@ class InfoCollector:
         return evaluations
 
     def get_categories(self):
+        self._get_url()
         elem_ranking_items = self._get_ranking_items()
         categories = tag_elems_list(elem_ranking_items, "dt")
         return categories[0]
 
-    def get_rankings(self):
+    def get_rankings(self, query_str):
+        self._get_url(query_str)
         elem_ranking_items = self._get_ranking_items()
         rankings = class_elems_list(elem_ranking_items, "is_rank")
         return rankings
 
-    def get_comments(self):
+    def get_comments(self, query_str):
+        self._get_url(query_str)
         elem_ranking_items = self._get_ranking_items()
         comments = class_elems_list(elem_ranking_items, "comment")
         return comments
 
-    def export_csv(self, titles, evaluations, categories, rankings, path):
+    def export_csv(self, titles, evaluations, rankings, categories, path):
         df = pd.DataFrame()
         df["観光地"] = titles
         df["総合評価"] = evaluations
--- test/test_info_collector.py
+++ test/test_info_collector.py
@@ -7,11 +7,21 @@ from os import path
 from info_collector import InfoCollector
 
 class TestInfoCollector(unittest.TestCase):
-    def test_get_titles(self):
+    def setUp(self):
+        self.info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/")
         titles = []
+        evaluations = []
+        rankings = []
         for i in range(1, 4):
-            info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(i))
-            titles.append(info_collector.get_titles())
+            titles.append(self.info_collector.get_titles("?page={}".format(i)))
+            evaluations.append(self.info_collector.get_evaluations("?page={}".format(i)))
+            rankings.append(self.info_collector.get_rankings("?page={}".format(i)))
+        self.titles = sum(titles, [])
+        self.evaluations = sum(evaluations, [])
+        self.rankings = sum(rankings, [])
+        self.categories = self.info_collector.get_categories()
+
+    def test_get_titles(self):
         self.assertEqual([
             "観光地 1",
             "観光地 2",
@@ -43,28 +53,20 @@ class TestInfoCollector(unittest.TestCase):
             "観光地 28",
             "観光地 29",
             "観光地 30",
-        ], sum(titles, []))
+        ], self.titles)
 
     def test_get_evaluations(self):
-        evaluations = []
-        for i in range(1, 4):
-            info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(i))
-            evaluations.append(info_collector.get_evaluations())
         self.assertEqual([
             4.7, 4.7, 4.6, 4.5, 4.5, 4.4, 4.3, 4.3, 4.2, 4.1,
             4.1, 4.0, 3.9, 3.9, 3.8, 3.7, 3.7, 3.6, 3.5, 3.5,
             3.4, 3.3, 3.3, 3.2, 3.1, 3.1, 3.0, 2.9, 2.9, 2.8
-        ], sum(evaluations, []))
+        ], self.evaluations)
 
     def test_get_categories(self):
-        info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/")
-        self.assertEqual(["楽しさ", "人混みの多さ", "景色", "アクセス"], info_collector.get_categories())
+        self.categories = self.info_collector.get_categories()
+        self.assertEqual(["楽しさ", "人混みの多さ", "景色", "アクセス"], self.categories)
 
     def test_get_rankings(self):
-        rankings = []
-        for i in range(1, 4):
-            info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(i))
-            rankings.append(info_collector.get_rankings())
         self.assertEqual([
             [4.6, 4.5, 4.9, 4.2],
             [4.6, 4.5, 4.9, 4.2],
@@ -96,31 +98,17 @@ class TestInfoCollector(unittest.TestCase):
             [2.8, 3.2, 3.4, 2.4],
             [2.8, 3.1, 3.4, 2.4],
             [2.7, 3.1, 3.3, 2.3]
-        ], sum(rankings, []))
+        ], self.rankings)
 
     # Comments are shown at random every time the browser is booted, so the value of each element cannot be tested.
     def test_get_comments(self):
         comments = []
         for i in range(1, 4):
-            info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(i))
-            comments.append(info_collector.get_comments())
+            comments.append(self.info_collector.get_comments("?page={}".format(i)))
         self.assertEqual(30, len(sum(comments, [])))
 
     def test_export_csv(self):
-        tmp_titles = []
-        tmp_evaluations = []
-        tmp_rankings = []
-        for i in range(1, 4):
-            info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/?page={}".format(i))
-            tmp_titles.append(info_collector.get_titles())
-            tmp_evaluations.append(info_collector.get_evaluations())
-            tmp_rankings.append(info_collector.get_rankings())
-        titles = sum(tmp_titles, [])
-        evaluations = sum(tmp_evaluations, [])
-        rankings = sum(tmp_rankings, [])
-        info_collector = InfoCollector("https://scraping-for-beginner.herokuapp.com/ranking/")
-        categories = info_collector.get_categories()
-        info_collector.export_csv(titles, evaluations, categories, rankings, "../csv/tour_reviews.csv")
+        self.info_collector.export_csv(self.titles, self.evaluations, self.rankings, self.categories, "../csv/tour_reviews.csv")
         self.assertEqual(True, path.exists("../csv/tour_reviews.csv"))
 
 if __name__ == "__main__":
